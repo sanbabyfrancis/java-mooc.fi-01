ea48c5dd2384fea858ee052740aa0a0368b4e3f9
San Baby Francis <san.baby.francis123@gmail.com>
2023-07-18 00:46:39 +0530

    complete exercise 02.13

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,22 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
+        int count = 0;
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number > 0) {
+                sum = sum + number;
+                count = count + 1;
+            }
+            if (number == 0) {
+                break;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((double)sum / count);
+        }
     }
 }
