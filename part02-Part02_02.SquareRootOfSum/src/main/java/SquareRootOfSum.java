1103bc50ee13ac65f8eda31c8848156e874d7403
San Baby Francis <san.baby.francis123@gmail.com>
2023-07-17 12:20:40 +0530

    complete exercise 02.04

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,9 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+        System.out.println(Math.sqrt(first + second));
 
     }
 }
