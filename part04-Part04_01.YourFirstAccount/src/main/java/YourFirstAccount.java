af53ce7a3cbf25ae6c55533145b596067c5b3ef4
San Baby Francis <san.baby.francis123@gmail.com>
2023-08-07 22:19:19 +0530

    complete exercise 04.05

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account myNewAccount = new Account("San's account", 100.0);
+        myNewAccount.deposit(20.0);
+        System.out.println(myNewAccount);
     }
 }
--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,11 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account newMatthewsAccount = new Account("Matthews account", 1000);
+        Account myNewAccount = new Account("My account", 0);
+        newMatthewsAccount.withdrawal(100.0);
+        myNewAccount.deposit(100.0);
+        System.out.println(newMatthewsAccount);
+        System.out.println(myNewAccount);
     }
 }
