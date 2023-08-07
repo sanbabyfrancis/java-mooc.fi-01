401e155ea2ee76fdefdcb55d3c84d7cedbbb5b2f
San Baby Francis <san.baby.francis123@gmail.com>
2023-08-07 22:29:37 +0530

    complete exercise 04.07

---
+++ part04-Part04_07.Product/src/main/java/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    private double price;
+    private int quantity;
+    private String name;
+    public Product(String initialName, double initialPrice, int initialQuantity) {
+        this.price = initialPrice;
+        this.name = initialName;
+        this.quantity = initialQuantity;
+    }
+    public void printProduct() {
+        System.out.println(this.name + ", " + "price " + this.price + ", " + this.quantity + " pcs");
+    }
+}
