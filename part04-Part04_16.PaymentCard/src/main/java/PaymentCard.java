a7a7a18d4461a96cd60b9387724cb4a03fe7fc8e
San Baby Francis <san.baby.francis123@gmail.com>
2023-08-09 22:00:25 +0530

    complete exercise 04.16

---
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -0,0 +1,34 @@
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;
+    }
+
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros";
+    }
+
+    public void eatAffordably() {
+        // write code here
+        if (this.balance - 2.60 >= 0) {
+            this.balance = this.balance - 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        // write code here
+        if (this.balance - 4.60 >= 0) {
+            this.balance = this.balance - 4.60;
+        }
+    }
+
+    public void addMoney(double amount) {
+        // write code here
+        if (this.balance + amount > 150.0) {
+            this.balance = 150.0;
+        } else if (amount > 0) {
+            this.balance = this.balance + amount;
+        }
+    }
+}
