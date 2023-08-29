f3effe28fc316276197970a03cfdb25f197b54b9
San Baby Francis <san.baby.francis123@gmail.com>
2023-08-30 02:03:17 +0530

    complete exercise 05.10

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,18 @@
 
 public class HealthStation {
-
+    private int count;
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        this.count = this.count + 1;
+        return person.getWeight();
     }
 
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    public int weighings() {
+        return this.count;
+    }
 }
--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -9,20 +9,64 @@ public class PaymentTerminal {
 
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment >= 2.50) {
+            this.affordableMeals++;
+            this.money += 2.50;
+            return (payment - 2.50);
+        } else {
+            return payment;
+        }
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment >= 4.30) {
+            this.heartyMeals++;
+            this.money += 4.30;
+            return (payment - 4.30);
+        } else {
+            return payment;
+        }
+    }
+
+    public boolean eatAffordably(PaymentCard card) {
+        // an affordable meal costs 2.50 euros
+        // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
+        // otherwise false is returned
+        if (card.takeMoney(2.50)) {
+            this.affordableMeals++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean eatHeartily(PaymentCard card) {
+        // a hearty meal costs 4.30 euros
+        // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
+        // otherwise false is returned
+        if (card.takeMoney(4.30)) {
+            this.heartyMeals++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum >= 0) {
+            card.addMoney(sum);
+            this.money += sum;
+        }
     }
 
 
